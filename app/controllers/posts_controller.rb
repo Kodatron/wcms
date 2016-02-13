8f08971476ad5687b382c48780744b3f75bdaf47
Johannes Nyberg <johannes.nyb@gmail.com>
2016-02-13 18:18:47 +0100

    Fixed bug with template after validation error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
         format.html { redirect_to admin_blog_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new }
+        format.html { render layout: 'layouts/admin', action: :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end

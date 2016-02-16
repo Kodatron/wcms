19e4979f7c2220790e72173766f7f89ef01b17c5
Johannes Nyberg <johannes.nyb@gmail.com>
2016-02-16 22:15:08 +0100

    Removed alot of files and code

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,63 +1,25 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :set_profile, only: [:edit, :update, :destroy]
 
-  # GET /profiles
-  # GET /profiles.json
-  def index
-    @profiles = Profile.all
-  end
-
-  # GET /profiles/1
-  # GET /profiles/1.json
-  def show
-  end
-
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
-  end
-
-  # GET /profiles/1/edit
   def edit
   end
 
-  # POST /profiles
-  # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
-
-    respond_to do |format|
-      if @profile.save
-        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
-        format.json { render :show, status: :created, location: @profile }
-      else
-        format.html { render :new }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
+    if @profile.save
+      # TODO: Change path when current_user is fixed
+      redirect_to users_path, notice: 'Profile was successfully created.'
+    else
+      render status: 402, action: :new
     end
   end
 
-  # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
   def update
-    respond_to do |format|
-      if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @profile }
-      else
-        format.html { render :edit }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
-  def destroy
-    @profile.destroy
-    respond_to do |format|
-      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
-      format.json { head :no_content }
+    if @profile.update(profile_params)
+      # TODO: Change path when current_user is fixed
+      redirect_to users_path, notice: 'Profile was successfully updated.'
+    else
+      render status: 402, action: :edit
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,36 +19,24 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render status: 402, action: :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render status: 402, action: :edit
     end
   end
 
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to admin_users_path, notice: 'User was successfully destroyed.'
   end
 
   private
@@ -59,10 +47,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(
-      :name,
-      :email,
-      :password,
-      :password_confirmation,
+      :name, :email, :password, :password_confirmation,
       profile_attributes: [:user_id, :firstname, :lastname, :phone, :wow_region, :wow_server, :wow_class]
       )
     end

70e9c47a7e476ed115610f7d025d58ded3f53beb
Johannes Nyberg <johannes.nyb@gmail.com>
2016-02-24 17:21:55 +0100

    #13 added migration file

--- db/migrate/20160224161614_create_alts.rb
+++ db/migrate/20160224161614_create_alts.rb
@@ -1,7 +1,13 @@
 class CreateAlts < ActiveRecord::Migration
   def change
     create_table :alts do |t|
-
+      t.integer :user_id
+      t.string :wow_name
+      t.integer :wow_class, limit: 1, default: 0
+      t.integer :wow_region, limit: 1, default: 0
+      t.integer :wow_server, limit: 1, default: 0
+      t.string :wow_spec
+      t.string :avatar
       t.timestamps null: false
     end
   end
